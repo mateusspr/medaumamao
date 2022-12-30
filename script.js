a3986ff8db290ea177bd9f48cdc2ae7601290501
Mateus Pereira Rodrigues <86198294+mateusmaiadb@users.noreply.github.com>
2022-12-30 14:47:27 -0300

    ajustes realizados

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function toggleMode(){
+function toggleMode() {
   const html = document.documentElement
 
-  html.classList.toggle('light')
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-dark.svg")
+  } else {
+    img.setAttribute("src","./assets/Avatar.svg")
+  }
 }
